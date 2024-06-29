be63f1770ae2698cdbf5987ac0ac2b610baacda5
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-06-29 23:04:52 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2032 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -8,31 +8,31 @@ int main()
     cin.tie(0);
     ios::sync_with_stdio(0);
 
-    int n, m;
-    cin >> n >> m;
-    m--;
+    int n, target;
+    cin >> n >> target;
+    target--;
     for (int i = 0; i < n; i++)
     {
         int index;
         cin >> index >> medals[index-1][0] >> medals[index-1][1] >> medals[index-1][2];
     }
 
-    int count = 0;
+    int count = n;
     for (int i = 0; i < n; i++)
     {
-        if (medals[m][0] > medals[i][0])
+        if (medals[target][0] > medals[i][0])
         {
-            count++;
+            count--;
         }
-        else if (medals[m][0] == medals[i][0])
+        else if (medals[target][0] == medals[i][0])
         {
-            if (medals[m][1] > medals[i][1])
+            if (medals[target][1] > medals[i][1])
             {
-                count++;
+                count--;
             }
-            else if (medals[m][1] == medals[i][1]){
-                if (medals[m][2] > medals[i][2]){
-                    count++;
+            else if (medals[target][1] == medals[i][1]){
+                if (medals[target][2] >= medals[i][2]){
+                    count--;
                 }
             }
         }
