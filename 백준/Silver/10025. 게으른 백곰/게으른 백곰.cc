44fdda8701da0f9593392b119c8cee5b295abe63
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-07-22 02:28:24 +0900

    [Silver III] Title: 게으른 백곰, Time: 152 ms, Memory: 6776 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.cc
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <queue>
 using namespace std;
 
 int main()
@@ -10,41 +11,46 @@ int main()
 
     int n, m;
     cin >> n >> m;
-    map<int, int> list;
-    int max_dis = 0;
-    int min_dis = 1000000000;
 
+    // 양동이를 거리별로 정렬시키기 위해 map 사용
+    map<int, int> list;
+
+    // 입력
     while (n--)
     {
-        int amount, distance;
+        int distance, amount;
         cin >> amount >> distance;
-        list[distance] += amount;
-        if (max_dis < distance)
-            max_dis = distance;
-        if (min_dis > distance)
-            min_dis = distance;
+        list[distance] = amount;
     }
 
-    int window_size = 2 * m + 1;
+    // 첫 양동이와 끝 양동이의 위치
+    int start = list.begin()->first;
+    int end = list.rbegin()->first;
+
+    // 초기 temp_sum
     int temp_sum = 0;
-    int max_sum = 0;
-
-    // Initial window sum
-    for (int i = min_dis - m; i <= min_dis + m; ++i)
+    for (int i = start; i <= start + 2 * m; i++)
     {
+        // 해당위치에 양동이가 있을 경우 더해줌
         if (list.count(i))
+        {
             temp_sum += list[i];
+        }
     }
-    max_sum = temp_sum;
+    int max_sum = temp_sum;
 
-    // Slide the window from min_dis to max_dis
-    for (int i = min_dis + 1; i <= max_dis; ++i)
+    // window silding기법
+    for (int i = start + 1; i <= end - m; i++)
     {
-        if (list.count(i - m - 1))
-            temp_sum -= list[i - m - 1];
-        if (list.count(i + m))
-            temp_sum += list[i + m];
+        // 닿지 못하는 양동이가 생긴 경우 temp_sum에 반영
+        if (list.count(i - 1))
+            temp_sum -= list[i - 1];
 
+        // 닿을 수 있는 양동이가 생긴 경우 temp_sum에 반영
+        if (list.count(i + 2 * m))
+            temp_sum += list[i + 2*m];
+
+        // max_sum 갱신
         if (max_sum < temp_sum)
             max_sum = temp_sum;
     }
