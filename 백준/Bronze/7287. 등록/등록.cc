97955a6883ed70f6dec5bdab17f651cd17d9c05a
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-08-14 16:00:33 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cout << 202 << "\n";
+    cout << "qktjwl123" << "\n";
+
+    return 0;
+}
