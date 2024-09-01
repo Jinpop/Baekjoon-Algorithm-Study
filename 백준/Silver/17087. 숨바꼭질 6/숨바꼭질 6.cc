87ba57b963af2277b61f33948f7b50fbba32bd18
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-09-01 18:34:40 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.cc
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.cc
@@ -1,49 +1,52 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
+
 using namespace std;
 
-int euclid(int a, int b)
-{
-    int x = max(a, b);
-    int y = min(a, b);
-    if (min(a, b) == 0)
-    {
-        return a;
+int gcd(int a, int b){
+    while(b != 0){
+        int r = a % b;
+        a = b;
+        b = r;
     }
-    else
-    {
-        return euclid(y, x % y);
-    }
+    return a;
 }
 
-int main()
-{
+int gcd_multiple(const vector<int>& num) {
+    int result = num[0];
+    for (int x : num) {
+        result = gcd(result, x);
+        if (result == 1) {
+            break; 
+        }
+    }
+    return result;
+}
+
+int main() {
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(0);
 
-    int n, m;
-    cin >> n >> m;
+    int n, s;
+    vector<int> num;
 
-    int before_num;
-    cin >> before_num;
-    int index = abs(before_num - m);
+    cin >> n >> s;
 
-    before_num = abs(before_num - m);
+    for(int i = 0 ; i < n ; i++){
+        int a;
 
-    for (int i = 1; i < n; i++)
-    {
-        int temp;
-        cin >> temp;
-        int new_num = euclid(abs(temp - m), before_num);
-        if (new_num < index)
-        {
-            index = new_num;
+        cin >> a;
+
+        int y = a - s;
+
+        if(y < 0){
+            y *= -1;
         }
-        before_num = abs(temp - m);
-    }
 
-    cout << index;
+        num.push_back(y);
+    }
+    cout << gcd_multiple(num);
 
     return 0;
 }
