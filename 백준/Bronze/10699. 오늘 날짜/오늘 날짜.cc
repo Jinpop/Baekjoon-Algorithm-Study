5ed012baf258521b7fbb0ff719b37c58b9233ab7
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-09-09 08:03:10 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cout << "2024-09-09";
+
+    return 0;
+}
