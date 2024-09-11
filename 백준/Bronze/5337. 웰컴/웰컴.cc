304a14c03676dd9541d1fe597b979d0eb833c6d1
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-09-11 18:50:28 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cout << ".  .   .\n";
+    cout << "|  | _ | _. _ ._ _  _\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+
+
+    return 0;
+}
