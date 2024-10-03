982c6237a40a022b808e52810748f71dca25a0bc
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-10-03 14:09:40 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cout << "고려대학교";
+
+    return 0;
+}
