e258137a4f5b288743aec9806b84c1f796d08c42
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-10-14 17:10:36 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+
+    if (n)
+    {
+        cout << "Leading the Way to the Future";
+    }
+    else {
+        cout << "YONSEI";
+    }
+
+    return 0;
+}
