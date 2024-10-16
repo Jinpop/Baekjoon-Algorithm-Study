7888b80ad89e7fd4a5a4aadc8a898f37847e6447
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-10-16 15:44:35 +0900

    [Gold V] Title: 창영이와 커피, Time: 76 ms, Memory: 80932 KB -BaekjoonHub

--- 백준/Gold/22115. 창영이와 커피/창영이와 커피.cc
+++ 백준/Gold/22115. 창영이와 커피/창영이와 커피.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
+#include <queue>
+#include <string>
+#include <set>
 using namespace std;
 
 int dp[101][100001];
@@ -17,7 +19,7 @@ int main()
     cin >> n >> m;
 
     vector<int> v;
-    v.push_back(0);  // 1-based indexing을 위해 0 추가
+    v.push_back(0);
 
     for (int i = 0; i < n; i++)
     {
@@ -28,34 +30,31 @@ int main()
 
     sort(v.begin(), v.end());
 
-    // dp, sum 배열을 초기화
     for (int i = 0; i <= n; i++)
     {
         for (int j = 0; j <= m; j++)
         {
-            dp[i][j] = 1e9; // 최대값으로 초기화
-            sum[i][j] = 0;
+            dp[i][j] = 1e9;
         }
     }
-    dp[0][0] = 0;  // 0을 만드는 데 필요한 항목 수는 0
 
-    // dp와 sum 채우기
+    dp[0][0] = 0;
+
     for (int i = 1; i <= n; i++)
     {
         for (int j = 0; j <= m; j++)
         {
-            dp[i][j] = dp[i - 1][j];  // i번째 아이템을 선택하지 않은 경우
+            dp[i][j] = dp[i - 1][j];
             sum[i][j] = sum[i - 1][j];
 
-            if (j >= v[i] && dp[i - 1][j - v[i]] + 1 < dp[i][j])
+            if (v[i] <= j && dp[i - 1][j - v[i]] + 1 < dp[i][j])
             {
                 dp[i][j] = dp[i - 1][j - v[i]] + 1;
                 sum[i][j] = sum[i - 1][j - v[i]] + v[i];
             }
         }
     }
-    
-    // 목표 금액을 만들 수 있는지 확인
+
     if (sum[n][m] == m)
     {
         cout << dp[n][m];
