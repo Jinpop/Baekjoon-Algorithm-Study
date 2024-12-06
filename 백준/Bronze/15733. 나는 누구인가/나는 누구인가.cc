8172a7c63699d504af27826dfda31772014b165a
Jinpop <112470541+Jinpop@users.noreply.github.com>
2024-12-06 18:14:26 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cout << "I'm Sexy";
+
+    return 0;
+}
