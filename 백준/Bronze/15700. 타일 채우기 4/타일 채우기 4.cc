61af049402a04c2b9d14ef80722738616e7efca5
Jinpop <112470541+Jinpop@users.noreply.github.com>
2025-01-12 23:11:40 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    long long a,b;
+    cin >> a >> b;
+
+    cout << a*b/2;
+
+    return 0;
+}
