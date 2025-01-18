9b281b4600c3c357678ec9dbe253b7961f3416bf
Jinpop <112470541+Jinpop@users.noreply.github.com>
2025-01-18 18:18:59 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cout << "파이팅!!";
+
+    return 0;
+}
