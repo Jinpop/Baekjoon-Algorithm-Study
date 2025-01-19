a611fb6e3c881be7982a727801447c63a4ed3987
Jinpop <112470541+Jinpop@users.noreply.github.com>
2025-01-20 05:28:28 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cout << fixed;
+    cout.precision(1);
+
+    double a, b;
+    cin >> a >> b;
+    
+    cout << a*b/2;
+
+    return 0;
+}
