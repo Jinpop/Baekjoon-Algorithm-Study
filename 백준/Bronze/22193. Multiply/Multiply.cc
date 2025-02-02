08653e95acc121da894450e6991431f57127c544
Jinpop <112470541+Jinpop@users.noreply.github.com>
2025-02-03 01:32:06 +0900

    [Bronze V] Title: Multiply, Time: 0 ms, Memory: 2020 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int a, b;
+    cin >> a >> b >> a >> b;
+
+    cout << a*b;
+
+    return 0;
+}
