1e1ff5979d0baa642229e863181773ca3a76372f
박성진 <qktjwl123@gmail.com>
2026-04-13 22:08:37 +0900

    [Silver I] Title: 후보 추천하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.cc
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.cc
@@ -1,67 +1,68 @@
-#include <iostream>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<long double, int> pdi;
+
+#define F(i, n) for (ll i = 1; i <= n; i++)
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define INF 1e9
+#define MX 101
+
+pii cnt[MX];
+bool compare(pair<pii, int> a, pair<pii, int> b)
+{
+    if (a.first.first < b.first.first)
+        return 1;
+    if (a.first.first > b.first.first)
+        return 0;
+
+    if (a.first.second < b.first.second)
+        return 1;
+    return 0;
+}
 
 int main()
 {
-    map<int, pair<int, int>> pic;
+    fastio;
 
-    int size, n;
-    cin >> size >> n;
+    int m, n;
+    cin >> m >> n;
 
-    for (int i = 1; i <= n; i++)
+    int total = 0;
+    F(i, n)
     {
-        int temp;
-        cin >> temp;
+        int t;
+        cin >> t;
 
-        // 입력 값이 존재하지 않을 경우
-        if (pic.find(temp) == pic.end())
+        if (cnt[t].first == 0)
+            total++;
+
+        if (total > m)
         {
-            // 사진틀에 공간이 있는 경우
-            if (pic.size() < size)
-            {
-                // 추천수를 올린다.
-                pic[temp].first = 1;
-                // 들어온 순서를 기록한다.
-                pic[temp].second = i;
-            }
-            else
-            {
-                pair<int, int> min;
-                int min_index = pic.begin()->first;
-                min = pic.begin()->second;
+            vector<pair<pii, int>> v;
+            F(i, 100)
+            if (cnt[i].first >= 1)
+                v.push_back({cnt[i], i});
 
-                // pic의 원소를 순회한 뒤 가장 추천수가 작으면서 들어온 순서가 오래된 인덱스를 찾는다.
-                for (auto item : pic)
-                {
-                    // pair의 대소 비교는 첫번째부터 진행되고 같을 경우 두번째를 비교한다.
-                    // 즉 추천수를 비교한 후 들어온 순서를 비교한다.
-                    if (item.second < min)
-                    {
-                        min_index = item.first;
-                        min = item.second;
-                    }
-                }
-
-                pic.erase(min_index);
-
-                // 추천 수를 올린다.
-                pic[temp].first = 1;
-                // 들어온 순서를 기록한다.
-                pic[temp].second = i;
-            }
+            sort(v.begin(), v.end(), compare);
+            cnt[v[0].second] = {0, 0};
+            total--;
         }
-        // 입력 값이 존재할 경우 값만 올려주면 된다.
-        else
-        {
-            pic[temp].first++;
-        }
+
+        cnt[t].first++;
+        if (cnt[t].first == 1)
+            cnt[t].second = i;
     }
 
-    for (auto item : pic)
-    {
-        printf("%d ", item.first);
-    }
+    F(i, 100)
+    if (cnt[i].first > 0)
+        cout << i << " ";
 
     return 0;
 }
+
+// 1 2
+// 3 4 5 6
