456de096b911191fbbd1f4138fcec33b1eb236c1
박성진 <qktjwl123@gmail.com>
2026-04-13 17:00:04 +0900

    [Silver II] Title: 주식, Time: 560 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -23,30 +23,15 @@ int main()
         cin >> n;
 
         int num[MX] = {0};
-        int cnt[10000] = {0};
         F(i, n)
-        {
-            cin >> num[i];
-            cnt[num[i]]++;
-        }
+        cin >> num[i];
 
         ll total = 0;
-        int idx = 10000;
-        int prev_i = 1;
-        F(i, n)
+        int mx = 0;
+        for (int i = n; i > 0; i--)
         {
-            while (cnt[idx] <= 0 && idx != 0)
-                idx--;
-
-            if (num[i] == idx)
-            {
-                for (int j = prev_i; j <= i; j++)
-                {
-                    total += idx - num[j];
-                    cnt[num[j]]--;
-                }
-                prev_i = i + 1;
-            }
+            mx = max(num[i], mx);
+            total += mx - num[i];
         }
 
         cout << total << "\n";
